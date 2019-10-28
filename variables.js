8784f7ed5400154864b3df97244c3dad5548b2aa
Danilo Nikolic <nikolicdanilo1410@gmail.com>
2019-10-27 22:05:08 -0700

    sunday

--- variables.js
+++ variables.js
@@ -1,5 +1,18 @@
 let deck = "abcdefghiabcdefghi"
+let deck2 = [`a<img src="./images/banana.jpg" />`]
 var boo = new Audio("./boo.mp3")
 var win = new Audio("./Applause.mp3")
 var flips = [boo, win]
 var resetButton
+let counter //to stop the interval
+let counter2 //to stop the interval
+let secs = 0
+let sec = 0
+let turn = 0
+let pairs = 0
+let timerbonus = 0
+let turnbonus = 0
+let timerRunning = false
+var startTime = new Date()
+var result = timerbonus + turnbonus
+var outcome = ""
